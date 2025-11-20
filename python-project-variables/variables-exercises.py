f01e5602717485bc1d28f293bf030516203cc13b
ediaz13 <diazemanuel27@gmail.com>
2025-11-20 16:42:56 -0300

    Python Variables Exercises

---
+++ python-project-variables/variables-exercises.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# Python Variables - Exercise #1
+x = 10
+x
+
+# Python Variables - Exercise #2
+x = 10
+print(x)
+
+# Python Variables - Exercise #3
+a, b, c, d = (10, 20, 30, 40)
+
+# Python Variables - Exercise #4
+a, b, c, d = (10, 20, 30, 40)
+print(b)
+print(d)
+
