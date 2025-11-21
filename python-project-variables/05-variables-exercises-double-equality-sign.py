f3a00b7339dbd82f3ce641694cbca308e1e8ce57
ediaz13 <diazemanuel27@gmail.com>
2025-11-21 11:39:52 -0300

    04 - Arithmetic Operators

---
+++ python-project-variables/05-variables-exercises-double-equality-sign.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#Arithmetic Operators - Exercise #1
+15 + 23
+
+#Arithmetic Operators - Exercise #2
+26 - 50
+
+#Arithmetic Operators - Exercise #3
+20 / 4
+
+#Arithmetic Operators - Exercise #4
+x = 22 / 4
