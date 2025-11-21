3a2e2743d29fec5904803776cdd6778f6264d192
ediaz13 <diazemanuel27@gmail.com>
2025-11-21 10:18:27 -0300

    03 - Strings

---
+++ python-project-variables/03-variables-exercises-strings.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#Strings - Exercise #1
+m = 100
+print("%d days" % m)
+
+#Strings - Exercise #2
+print("It's cool, isn't it?")
+
+#Strings - Exercise #3
+"Don't be shy"
+
+#Strings - Exercise #4
+print('Click "OK"')
+
+#Strings - Exercise #5
+print('Big', 'Houses')
