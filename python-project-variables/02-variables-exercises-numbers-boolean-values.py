9fde5fc9bcb17e81cde1789b63a0e04391f4fa8f
ediaz13 <diazemanuel27@gmail.com>
2025-11-21 00:14:04 -0300

    Numbers and Boolean Values

---
+++ python-project-variables/02-variables-exercises-numbers-boolean-values.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#Numbers and Boolean Values - Exercise #1
+a = True
+type(a)
+
+#Numbers and Boolean Values - Exercise #2
+b = 99
+type(b)
+
+#Numbers and Boolean Values - Exercise #3
+type(0.99)
+
+#Numbers and Boolean Values - Exercise #4
+b = 99
+float(b)
+print(b)
+
+#Numbers and Boolean Values - Exercise #5
+int(0.99)
