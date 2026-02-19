2aaad7b2cdb559c33d2bf556346d846ba605c4aa
ediaz13 <diazemanuel27@gmail.com>
2026-02-18 21:21:42 -0300

    Seccion 15: Conditional Statements

---
+++ python-project-variables/42-conditional-statements.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#The IF Statement - Exercise #1
+if 5 > 2:
+    print("The condition has been satisfied")
+
+#The IF Statement - Exercise #2
+x = 10
+y = 25
+
+if x > 3 and y > 13:
+    print("Both conditions are correct")
+if x <= 3 or y <= 13:
+    print("At least one of the conditions is false")
